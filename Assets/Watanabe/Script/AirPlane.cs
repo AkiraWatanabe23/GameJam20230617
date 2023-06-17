fa85372d818c820cfc77a70dcdc6f040fe40d282
Akira Watanabe <a0323w@gmail.com>
2023-06-17 11:18:16 +0900

    [add] 中心を起点に円運動するオブジェクト

---
+++ Assets/Watanabe/Script/AirPlane.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class AirPlane : MonoBehaviour
+{
+    [SerializeField] private Transform _center = default;
+    [SerializeField] private float _radius = 1f;
+
+    private float _theta = 0f;
+
+    private void Start()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        float x = _radius * Mathf.Cos(_theta);
+        float z = _radius * Mathf.Sin(_theta);
+
+        transform.position = new Vector3(x, 0, z) + _center.position;
+        _theta += 3f * (2f * Mathf.PI / 360f);
+    }
+}
