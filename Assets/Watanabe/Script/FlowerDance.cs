7c875360c9aeffb0c0917f44644ea9a61cd3fa5d
Akira Watanabe <a0323w@gmail.com>
2023-06-17 17:46:42 +0900

    [update] 可視判定

--- Assets/Watanabe/Script/FlowerDance.cs
+++ Assets/Watanabe/Script/FlowerDance.cs
@@ -1,22 +1,22 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
 using Usugi;
+using UniRx;
+using UniRx.Triggers;
 
 /// <summary> 敵の動き </summary>
 public class FlowerDance : MonoBehaviour, IEnemyBehavior
 {
-    [SerializeField] private Image _sabotagePanel = default;
     [Tooltip("妨害する秒数")]
     [SerializeField] private float _sabotage = 1f;
+    [SerializeField] private Renderer _renderer = default;
+
+    private void Start()
+    {
+        _renderer.OnBecameVisibleAsObservable().Subscribe(_ => EnemyBehavior());
+    }
 
     public void EnemyBehavior()
     {
         StartCoroutine(GameSceneUI.Instance.Sabotage(_sabotage));
-    }
-
-    private void OnBecameVisible()
-    {
-        Debug.Log("みえてる");
-        EnemyBehavior();
     }
 }
