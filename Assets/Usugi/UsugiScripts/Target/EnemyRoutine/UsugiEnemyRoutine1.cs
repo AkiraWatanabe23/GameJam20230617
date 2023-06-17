7610eef79dc398e83c144330ff48d1b9fc042d65
TsuyosiUsugi <qesulive@gmail.com>
2023-06-17 12:10:52 +0900

    [add] エネミーの動きのインターフェース完成

---
+++ Assets/Usugi/UsugiScripts/Target/EnemyRoutine/UsugiEnemyRoutine1.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Usugi;
+
+public class UsugiEnemyRoutine1 : MonoBehaviour, IEnemyBehavior
+{
+    public void EnemyBehavior()
+    {
+    }
+}
---
+++ Assets/Usugi/UsugiScripts/Target/IEnemyBehavior.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Usugi
+{
+    /// <summary>
+    /// 敵の行動用のインターフェース
+    /// </summary>
+    public interface IEnemyBehavior
+    {
+        void EnemyBehavior();
+    }
+}
