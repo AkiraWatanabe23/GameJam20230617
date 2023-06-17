d33b821341fd78ad79e8f76f7642b57135bfa5de
Akira Watanabe <a0323w@gmail.com>
2023-06-17 14:59:45 +0900

    [add] Fade

---
+++ Assets/Watanabe/Script/Fade.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class Fade : MonoBehaviour
+{
+    [Tooltip("フェードさせるUI")]
+    [SerializeField] private Image _fadePanel;
+    [Tooltip("実行時間")]
+    [SerializeField] private float _fadeTime = 1f;
+
+    [SerializeField] private UnityEvent _onCompleteFadeIn = default;
+    [SerializeField] private UnityEvent _onCompleteFadeOut = default;
+
+    public static Fade Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        if (_fadePanel)
+        {
+            StartFadeIn();
+        }
+    }
+
+    public void StartFadeIn()
+    {
+        StartCoroutine(FadeIn());
+    }
+
+    public void StartFadeOut()
+    {
+        StartCoroutine(FadeOut());
+    }
+
+    private IEnumerator FadeIn()
+    {
+        _fadePanel.gameObject.SetActive(true);
+
+        //α値(透明度)を 1 -> 0 にする(少しずつ明るくする)
+        float alpha = 1f;
+        Color color = _fadePanel.color;
+
+        do
+        {
+            alpha -= Time.deltaTime * (1f / _fadeTime);
+            if (alpha <= 0f)
+            {
+                alpha = 0f;
+            }
+            color.a = alpha;
+            _fadePanel.color = color;
+            yield return null;
+        }
+        while (alpha > 0f);
+
+        _fadePanel.gameObject.SetActive(false);
+        //フェード後に実行する関数
+        _onCompleteFadeIn?.Invoke();
+    }
+
+    private IEnumerator FadeOut()
+    {
+        _fadePanel.gameObject.SetActive(true);
+
+        //α値(透明度)を 0 -> 1 にする(少しずつ暗くする)
+        float alpha = 0f;
+        Color color = _fadePanel.color;
+
+        do
+        {
+            alpha += Time.deltaTime * (1f / _fadeTime);
+            if (alpha >= 1f)
+            {
+                alpha = 1f;
+            }
+            color.a = alpha;
+            _fadePanel.color = color;
+            yield return null;
+        }
+        while (alpha < 1f);
+
+        //フェード後に実行する関数
+        _onCompleteFadeOut?.Invoke();
+    }
+}
